bc76dbff7b01c8b025db603ab5b8096c71f06342
Noah <noah.hemery@primever.com>
2024-08-06 14:24:06 +0200

    ✨Add Quill to edit text

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,7 +6,9 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -67,7 +69,11 @@ class ArticleType extends AbstractType
                 ],
                 'required' => false
             ])
-            ->add('content', CKEditorType::class)
+            ->add('content', HiddenType::class, [
+                'attr' => [
+                    'id' => 'content',
+                ]
+            ])
             ->add('enabled', CheckboxType::class, [
                 'label' => 'Activer l\'article',
                 'attr' => [
